86e865903e239504b7c811122c5b6492dfea2abd
Adam Havelka <linuxes@mineland.eu>
2017-12-20 22:11:30 +0100

    handling quitting players, dummy clients gets question

--- me.js
+++ me.js
@@ -2,6 +2,11 @@ let WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({port: 8124}),
     clients = new Map();
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync("./players.json");
+const db = low(adapter);
+
 const questions = require("./questions.json");
 
 function getRandomQuestion() {
@@ -18,38 +23,66 @@ wss.on('connection', function(ws) {
         let data = JSON.parse(message)
         console.log("datá", data)
         clients.set(data.nick, ws);
-
-        if(data.onlinePlayers){
-            let onlinePlayers = []
-            clients.forEach( (val, key) => {
-                if(key != data.nick){
-                    onlinePlayers.push({nick: key, score: 0, level: 0})
-                }
-            } )
-
-            //ws.send(JSON.stringify({players: onlinePlayers}))
-
-            sendAll();
-
+        let record = db.find({nick: data.nick}).value();
+        if(!record){
+            db.set({nick: data.nick, level:0, score:0}).write();
         }
 
-        if(data.chooseUser){
 
-            //send mesage to opponent
-            clients.get(data.opponent).send(JSON.stringify({offerGame: true, opponent: data.nick}))
+        try{
+
+            if(data.onlinePlayers){
+                let onlinePlayers = []
+                clients.forEach( (val, key) => {
+                    if(key != data.nick){
+                        onlinePlayers.push({nick: key, score: 0, level: 0})
+                    }
+                } )
+
+                //ws.send(JSON.stringify({players: onlinePlayers}))
+
+                sendAll();
+
+            }
+
+            if(data.chooseUser){
+
+                //send mesage to opponent
+                clients.get(data.opponent).send(JSON.stringify({offerGame: true, opponent: data.nick}))
+            }
+            if(data.acceptOpponent){
+                clients.get(data.opponent).send(JSON.stringify({gameAccepted: true, opponent: data.nick}))
+            }
+            if(data.refuseOpponent){
+                clients.get(data.opponent).send(JSON.stringify({gameRefused: true, opponent: data.nick}))
+            }
+
+            if(data.getQuestion){
+                let question = getRandomQuestion();
+                clients.get(data.nick).send(JSON.stringify({randomQuestion: true, opponent: data.opponent, question: question, realm: data.realm}))
+                clients.get(data.opponent).send(JSON.stringify({randomQuestion: true, opponent: data.nick, question: question, realm: data.realm}))
+            }
+
+            if(data.incrementUserLevel){
+                console.log("peru tam ten level")
+                let actualLevel = db.find({nick: data.nick}).value().level;
+                db.find({nick: data.nick}).set("level", actualLevel + 1).write();
+            }
+            if(data.quitting){
+                console.log("someone's leaving: ", data.nick)
+                clients.delete(data.nick)
+                clients.get(data.opponent).send(JSON.stringify({quitting: true, opponent: data.nick}))
+            }
+            if(data.lostGame){
+                clients.get(data.opponent).send(JSON.stringify({won: true, opponent: data.nick}))
+            }
         }
-        if(data.acceptOpponent){
-            clients.get(data.opponent).send(JSON.stringify({gameAccepted: true, opponent: data.nick}))
-        }
-        if(data.refuseOpponent){
-            clients.get(data.opponent).send(JSON.stringify({gameRefused: true, opponent: data.nick}))
+        catch(e){
+            //this one is no longer online
+            console.log(data.opponent, " this one is no longer online")
+            clients.delete(data.opponent)
         }
 
-        if(data.getQuestion){
-            let question = getRandomQuestion();
-            clients.get(data.nick).send(JSON.stringify({randomQuestion: true, opponent: data.opponent, question: question}))
-            clients.get(data.opponent).send(JSON.stringify({randomQuestion: true, opponent: data.nick, question: question}))
-        }
 
     });
 });
@@ -67,6 +100,12 @@ function sendAll () {
             }
         })
 
-        ws.send(JSON.stringify({players: response}));
+        try{
+            ws.send(JSON.stringify({players: response}));
+        }
+        catch(e){
+            //this one is no longer online
+            clients.delete(nick)
+        }
     } )
 }
